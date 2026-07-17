d7067f9835cddabc14c61d038943e6b808ff567f
Harish-Kuppusamy <ksharishcs20@gmail.com>
2026-07-17 14:43:13 +0530

    JS first class

---
+++ script.js
@@ -0,0 +1,42 @@
+//log output
+
+console.log(10);
+console.error("hello");
+console.warn("dont touch");
+
+// variables
+
+var a = 10;
+var a = 20;  //redeclare
+a = 30;    // reassign
+
+let a = "hello";
+let a = "world" // cant redeclare let 
+a = "india" // reassign
+
+const isLogged = true; // cant redeclare && reassign
+
+// Data Types - Primitive
+
+let name = "harish"  //string
+let age = 22 // Number
+let adult = true // Boolean
+let others; // undefined
+let z = null; // null
+
+//operators
+ 
+let a = 10 + 20;  // Addition
+let b = 50 - 20;  // Subtraction
+let c = 5 * 3;    // Multiplication
+let d = 100 / 4;  // Division
+let e = 11 % 3; // modulo
+
+let age = 22;
+++age // pre increment 
+age++ // post increment
+--age// pre decrement
+age-- // post decrement
+
+
+
